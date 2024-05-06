ade88767c923f972d1cdb2c28aac8c8ee7e10d8e
Farelito <bakoufarel@gmail.com>
2024-05-06 16:16:23 +0200

    0. Hello, print

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 #display the simple string
-print ('"Programming is like building a multilingual puzzle\n')
+print("\"Programming is like building a multilingual puzzle")
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 for i in range(90):
-    print(f"{i:02}", end="")
-    if i != 89:
+  for j in range(i + 1, 10):
+    print(f"{i}{j}", end="")
+    if i != 8 or j != 9:
         print(",", end="")
     else:
         print()
