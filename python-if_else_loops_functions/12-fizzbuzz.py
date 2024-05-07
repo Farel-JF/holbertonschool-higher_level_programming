68f6fe4df33ff76a8f69a5f1ce9b00d8838a102a
Farelito <bakoufarel@gmail.com>
2024-05-07 12:33:26 +0200

    check shit

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,11 @@
 def fizzbuzz():
   for i in range(1, 98):
     if i % 3 == 0 and i % 5 == 0:
-      print("FizzBuzz ")
+      print(f"FizzBuzz")
     elif i % 3 == 0:
       print("Fizz ")
     elif i % 5 == 0:
-      print("Buzz")
+      print(f"Buzz")
     else:
       print("{}".format(i))
     
--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(90):
   for j in range(i + 1, 10):
     print(f"{i}{j}", end="")
     if i != 8 or j != 9:
-        print(",", end="")
+        print(f",", end="")
     else:
-        print()
+        print(f"")
