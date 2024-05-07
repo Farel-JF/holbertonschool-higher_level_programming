bca4dc5cfab5db500f688f9a1946dc89a0b8179f
Farelito <bakoufarel@gmail.com>
2024-05-07 02:12:41 +0200

    check few task

--- python-hello_world/5-print_string.py
+++ python-hello_world/5-print_string.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print(str * 3)
-print(str[:9])
+print(str * 3 + '\n' + str[:9])
+
 
 
---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def uppercase(s):
+  for char in s:
+    if ord('a') <= ord(char) <= ord('z'):
+      uppercase_char = chr(ord(char) - ord('a') + ord('A'))
+    else:
+      uppercase_char = char
+      print(uppercase_char, end="")
+      print()
+
