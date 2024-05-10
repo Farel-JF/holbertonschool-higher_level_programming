804ae90187d2fd4ba634b5fc2063a5b4612dacb5
Farelito <bakoufarel@gmail.com>
2024-05-10 11:40:19 +0200

    2. How to make a script dynamic!

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    import sys
 
-    if (len(sys.argv) - 1) == 0:
-        print("{} arguments.".format((len(sys.argv) - 1)), end="\n\n")
-    elif (len(sys.argv) - 1) == 1:
-        print("{} argument:".format(len(sys.argv) - 1), end="\n")
-        print("{}: {}".format((len(sys.argv) - 1) ,(sys.argv[1:])[0]), end="\n\n")
-    else:
-        print("{} arguments:".format((len(sys.argv) - 1)), end="\n")
-        for i, arg in enumerate((sys.argv[1:]), start=1):
-            print("{}: {}".format(i, arg))
+import sys
+
+if __name__ == "__main__":
+  if (len(sys.argv) - 1) == 0:
+    print("{} arguments.".format((len(sys.argv) - 1)), end="\n\n")
+  elif (len(sys.argv) - 1) == 1:
+    print("{} argument:".format(len(sys.argv) - 1), end="\n")
+    print("{}: {}".format((len(sys.argv) - 1) ,(sys.argv[1:])[0]), end="\n\n")
+  else:
+    print("{} arguments:".format((len(sys.argv) - 1)), end="\n")
+  for i, arg in enumerate((sys.argv[1:]), start=1):
+    print("{}: {}".format(i, arg))
