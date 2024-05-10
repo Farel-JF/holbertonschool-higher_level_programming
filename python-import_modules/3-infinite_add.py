8e40027ce66597eeb09c793d6eb0aab309d1187a
Farelito <bakoufarel@gmail.com>
2024-05-10 12:39:43 +0200

    pycodestyle .03

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    sum = 0
-    result = sys.argv[1:]
-    for arg in result:  
-        sum += int(arg)
-    print("{}".format(sum))
+      import sys
+      sum = 0
+      result = sys.argv[1:]
+      for arg in result:  
+          sum += int(arg)
+      print("{}".format(sum))
