c503bb4994ce2b8641592ded780d05cfba81c1d0
Farelito <bakoufarel@gmail.com>
2024-05-10 13:24:31 +0200

    pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    if (len(sys.argv) - 1) == 0:
-        print("{} arguments.".format((len(sys.argv) - 1)), end="\n")
-    elif (len(sys.argv) - 1) == 1:
-        print("{} argument:".format(len(sys.argv) - 1), end="\n")
-    else:
-        print("{} arguments:".format((len(sys.argv) - 1)), end="\n")
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+        import sys
+        if (len(sys.argv) - 1) == 0:
+          print("{} arguments.".format((len(sys.argv) - 1)), end="\n")
+        elif (len(sys.argv) - 1) == 1:
+          print("{} argument:".format(len(sys.argv) - 1), end="\n")
+        else:
+          print("{} arguments:".format((len(sys.argv) - 1)), end="\n")
+        for i, arg in enumerate(sys.argv[1:], start=1):
+          print("{}: {}".format(i, arg))
