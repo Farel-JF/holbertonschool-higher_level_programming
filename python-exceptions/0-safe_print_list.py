3c4da6206aa39011840e2afdd6e39acb9387a675
Farelito <bakoufarel@gmail.com>
2024-05-15 01:28:41 +0200

    0. Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,6 @@ def safe_print_list(my_list=[], x=0):
   for i in range(x):
     try:
      print("{:d}".format(my_list[i]), end='')
-     count += 1
     except IndexError:
       pass
     count += 1
