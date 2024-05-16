bdf537058986fb8524168ab616e33ff122707e68
Farelito <bakoufarel@gmail.com>
2024-05-17 01:13:09 +0200

    12. Roman to Integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    """
+    Convert a Roman numeral string to an integer.
+
+    Args:
+        roman_string (str): The input Roman numeral string.
+
+    Returns:
+        int: The integer value corresponding to the Roman numeral string.
+    """
+
     if not  roman_string or None:
         return 0
 
