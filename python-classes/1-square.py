b6d1ae3e9d3f4691b4448db9bda9ef1136c3c6b5
Farelito <bakoufarel@gmail.com>
2024-05-16 14:33:04 +0200

    1. Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+""" contient le square"""
 class Square:
     def __init__(self, size):
         self.__size = size
+
+"""
+class Square:
+    def __init__(self, size):
+        self.__size = size
+"""
+
 square = Square(5)
