52594fccb7b9a5524dabccd02457b4b13937ac3a
Farelito <bakoufarel@gmail.com>
2024-05-16 16:49:32 +0200

    0. Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -4,6 +4,6 @@ def square_matrix_simple(matrix=[]):
     for row_matrix in matrix:
         new_row = []
         for num in row_matrix:
-           new_row.append(num ** 2)
+            new_row.append(num ** 2)
         new_matrix.append(new_row)
     return new_matrix
