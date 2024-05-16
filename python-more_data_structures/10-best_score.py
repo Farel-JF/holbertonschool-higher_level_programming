43ed20d17fd2c056cccb7f88d6897fc9c5281926
Farelito <bakoufarel@gmail.com>
2024-05-17 01:12:17 +0200

    10. Best score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -7,7 +7,8 @@ def best_score(a_dictionary):
         a_dictionary (dict): The input dictionary.
 
     Returns:
-        str or None: The key with the biggest integer value, or None if the dictionary is empty.
+        str or None: The key with the biggest integer value, or None if the
+        dictionary is empty.
     """
     if not a_dictionary:
         return None
