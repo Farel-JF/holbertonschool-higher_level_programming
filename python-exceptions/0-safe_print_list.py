a464357b250c8e14f87491b4eaf680eb0fe77133
Farelito <bakoufarel@gmail.com>
2024-05-17 10:49:44 +0200

    pycodestyle *.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-  count = 0
-  for i in range(x):
+    count = 0
     try:
-     print("{:d}".format(my_list[i]), end='')
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
     except IndexError:
-      break
-    count += 1
-  print()
-
-  return count
+        pass
+    finally:
+        print()
+    return count
--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -16,5 +16,4 @@ def list_division(my_list_1, my_list_2, list_length):
             print("wrong type")
         finally:
             New_list.append(result)
-            
     return New_list
