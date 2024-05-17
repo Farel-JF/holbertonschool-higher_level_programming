5bbb60cc3580dd47836e9f47e18c88757ccf6696
Farelito <bakoufarel@gmail.com>
2024-05-17 11:55:41 +0200

    pycodestyle *.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 def print_matrix_integer(matrix=[[]]):
-  for row_mtx in matrix:
+    for row_mtx in matrix:
         for i, num in enumerate(row_mtx):
             if i != 0:
                 print(" ", end='')
