648417bdc139d8a45e067e89ff811d64450924dc
Farelito <bakoufarel@gmail.com>
2024-05-17 11:35:35 +0200

    test pycodestayle

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -9,18 +9,14 @@ def roman_to_int(roman_string):
     Returns:
         int: The integer value corresponding to the Roman numeral string.
     """
-
     if not roman_string or None:
         return 0
-
     roman_numerals = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50,
         'C': 100, 'D': 500, 'M': 1000
     }
-
     result = 0
     prev_value = 0
-
     for char in reversed(roman_string):
         value = roman_numerals[char]
         if value < prev_value:
@@ -28,5 +24,4 @@ def roman_to_int(roman_string):
         else:
             result += value
         prev_value = value
-
     return result
