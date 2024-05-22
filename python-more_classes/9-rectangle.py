5145ce245bccd235add1e8c63192ccb7df182dd4
Farelito <bakoufarel@gmail.com>
2024-05-22 20:17:44 +0200

    """Rectangle"""

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -8,6 +8,8 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = '#'
 
+    """Rectangle"""
+    
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
