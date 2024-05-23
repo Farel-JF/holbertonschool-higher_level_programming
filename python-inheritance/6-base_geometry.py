9aded847f83ef43098d42ad48ce7a8aa12df23c8
Farelito <bakoufarel@gmail.com>
2024-05-23 13:44:08 +0200

    7. Integer validator & 6. Improve Geometry

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""def BaseGeometry class"""
+
+
 class BaseGeometry:
+    """def area"""
     def area(self):
         raise Exception("area() is not implemented")
-    
--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+"""def BaseGeometry class"""
+
+
 class BaseGeometry:
+    """def area"""
     def area(self):
         raise Exception("area() is not implemented")
+
+    """def integer_validator"""
+
     def integer_validator(self, name, value):
         if type(name) is chr:
             return
