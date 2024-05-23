77bc1755081448babb7ff9e6604e77b8067cacdc
Farelito <bakoufarel@gmail.com>
2024-05-23 13:52:57 +0200

    idk

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,7 +12,9 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Method to validate integer values"""
 
-        if type(name) is not int:
-            raise TypeError("{:s} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{:s} must be greater than 0".format(name))
+        if type(name) is chr:
+            return
+        elif not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
