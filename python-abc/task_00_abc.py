cd989765d0b7245bc48ed861c6a8ca3737b30770
Farelito <bakoufarel@gmail.com>
2024-05-24 17:48:34 +0200

    0. Abstract Animal Class and its Subclasses

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -5,15 +5,23 @@ This module contains classes representing different animals.
 
 from abc import ABC, abstractmethod
 
+# Abstract base class
 class Animal(ABC):
+
     @abstractmethod
     def sound(self):
         pass
 
+
+# Subclass Dog from the animal
 class Dog(Animal):
+    
     def sound(self):
         print("Bark")
 
+
+# Subclass cat from the animal
 class Cat(Animal):
+
     def sound(self):
-        print("Meow")
+        return "Meow"
