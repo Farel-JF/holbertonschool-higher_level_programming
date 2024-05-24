fae3308ec3cbdccceee36f3edd81341a9b3ba070
Farelito <bakoufarel@gmail.com>
2024-05-24 17:40:10 +0200

    5. The Mystical Dragon - Mastering Mixins

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -3,25 +3,27 @@
 This module contains classes representing Mystical Dragon.
 """
 
-#  swim
+# SwimMixin class for adding swimming functionality
 class SwimMixin:
     def swim(self):
         print("The creature swims!")
 
-# FlyMixin  method
+# FlyMixin class for adding flying functionality
 class FlyMixin:
     def fly(self):
         print("The creature flies!")
 
-
-# Dragon class inherit
+# Dragon class inherits from both SwimMixin and FlyMixin
 class Dragon(SwimMixin, FlyMixin):
     def roar(self):
         print("The dragon roars!")
 
+# Instantiate a Dragon object
 draco = Dragon()
 
-
+# Demonstrate abilities
 draco.swim()
 draco.fly()
 draco.roar()
+
+
