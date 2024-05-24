42121713cc8431cef1870ed616400e958d9f6850
Farelito <bakoufarel@gmail.com>
2024-05-24 19:04:18 +0200

    blccc

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-"""BaseGeometry class"""
+""""Class BaseGemotry"""
+
 
 class BaseGeometry:
-    """BaseGeometry class empty"""
+    """empty class BaseGeometry"""
 
     def area(self):
-        """Method to calculate the area of a geometric shape"""
-
+        """that raises an Exception with the message
+        area() is not implemented"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Method to validate integer values"""
+        """
+        Validates an integer value.
 
-        if type(name) is chr:
-            return
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+        Args:
+            name (str): The name of the value.
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
