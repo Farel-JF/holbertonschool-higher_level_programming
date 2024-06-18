a9680de5fd25501d25ebc477260c3dc8aabdd1e2
Farelito <bakoufarel@gmail.com>
2024-06-18 15:02:46 +0200

    11. Select the best

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,1 +1,4 @@
-SELECT score, name From second_table ORDER BY score DESC;
+-- This script lists all records from the table second_table ordered by score
+SELECT score, name
+FROM second_table
+ORDER BY score DESC;
