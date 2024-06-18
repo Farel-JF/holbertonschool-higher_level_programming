5c8d0839e31ea40fb10117867f3dbb45a7c9fbe4
Farelito <bakoufarel@gmail.com>
2024-06-18 15:37:16 +0200

    4. First table & 12. Cheating is bad

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,4 @@
+-- This script updates the score of Bob to 10 in second_table based on the name field
 UPDATE second_table
 SET score = 10
 WHERE name = 'Bob';
--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
--- the first table
-CREATE TABLE IF NOT EXISTS first_table(
-	id INT,
-	nama VARCHAR(256)
+-- This script creates the table first_table if it doesn't exist in the current database
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
 );
