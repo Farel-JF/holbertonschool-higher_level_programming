07e1659040db93ee62ca7c10b58ebb798b84ef8c
Farelito <bakoufarel@gmail.com>
2024-06-18 15:27:20 +0200

    comment addd to task 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
+-- This script lists records from second_table where name is not empty, ordered by descending score
 SELECT score, name
 FROM second_table
-where name IS NOT NULL AND name != ''
+WHERE name IS NOT NULL AND name != ''
 ORDER BY score DESC;
