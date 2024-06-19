dbc2d7fc9913ed76ccb2a702aac772e183aa38c5
Farelito <bakoufarel@gmail.com>
2024-06-19 14:05:53 +0200

    4. ID can't be null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,5 +1,6 @@
---- a script that creates the table force_name
-CREATE TABLE IF NOT EXISTS id_not_null(
-	id INT DEFAULT 1,
-	name VARCHAR(256)
+-- a script that creates the table force_name
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
 );
+
