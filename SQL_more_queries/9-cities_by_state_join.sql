c2032feb46c1e56e3f283311c176be377c06ca9b
Farelito <bakoufarel@gmail.com>
2024-06-19 15:10:06 +0200

    9. Cities by States

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,7 @@
 -- Select columns from cities and states tables
 -- Join cities table with states table based on state_id matching id
 -- Order the results by cities.id in ascending order
-SELECT cities.id AS cities_id , cities.name AS cities_name, states.name AS state_name
+SELECT cities.id AS id , cities.name AS name, states.name AS name
 	FROM cities
 	INNER JOIN states ON states.id = cities.state_id
 	ORDER BY cities.id
