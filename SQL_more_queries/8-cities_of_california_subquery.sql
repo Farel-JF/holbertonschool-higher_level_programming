125d9cb863b1c0baf1f19a76d9cffdb89b486abd
Farelito <bakoufarel@gmail.com>
2024-06-19 14:51:28 +0200

    8. Cities of California

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- Select the id of the state California
+-- Use the id to fetch cities of California, sorted by cities.id
+SELECT id FROM states WHERE name = 'California';
+SELECT * FROM cities
+WHERE state_id IN (SELECT id FROM states WHERE name = California)
+ORDER BY id ASC;
