b3b9132441013f14d9f1c676e18c21e3b69e6d07
Farelito <bakoufarel@gmail.com>
2024-06-19 14:25:01 +0200

    6. States table

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,7 +2,7 @@
 -- Create the table if it does not exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id INT AUTO_INCREMENT NOT NULL UNIQUE,
+    id INT AUTO_INCREMENT UNIQUE,
     name VARCHAR(256) NOT NULL,
 	PRIMARY KEY
 );
