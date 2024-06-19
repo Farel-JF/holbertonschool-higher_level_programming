796e0152607faed3a42408977779985c8b84b852
Farelito <bakoufarel@gmail.com>
2024-06-19 17:09:00 +0200

    0. Get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,7 @@ This script lists all states from the database hbtn_0e_0_usa
 """
 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
     # Check if all three arguments are provided
