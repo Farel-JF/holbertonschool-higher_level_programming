e80de677796633db9c3c535bf1703936e853b1c2
Farelito <bakoufarel@gmail.com>
2024-06-20 14:49:56 +0200

    7. All states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,56 +1,39 @@
 #!/usr/bin/python3
 """
-Script to list all cities of a specific state from the database hbtn_0e_4_usa
+Script to list all State objects from the database hbtn_0e_6_usa
 """
 
-import MySQLdb
-from sys import argv
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
-from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
-    state_name = argv[4]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    # Connect to MySQL database
-    db = MySQLdb.connect
-    (
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-    )
+    # Database connection string
+    db_string = f"mysql://{username}:{password}@localhost:3306/{database}"
 
-    # Create a cursor object to interact with the database
-    cur = db.cursor()
+    # Create database engine
+    engine = create_engine(db_string)
 
-    # Prepare SQL query to select all cities of the specified state, sorted by id
-    query = """
-            SELECT cities.id, cities.name, states.name
-            FROM cities
-            JOIN states ON cities.state_id = states.id
-            WHERE states.name = %s
-            ORDER BY cities.id ASC
-            """
-    cur.execute(query, (state_name,))
+    # Bind the engine to the Base class
+    Base.metadata.bind = engine
 
-    # Fetch all the rows returned by the query
-    cities = cur.fetchall()
+    # Create a session factory
+    DBSession = sessionmaker(bind=engine)
 
-    # Print each city
-    for city in cities:
-        print(city)
-    # Close cursor and connection
-    cur.close()
-    db.close()
+    # Create a session
+    session = DBSession()
 
-if __name__ == "__main__":
-    engine = create_engine(
-                'mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1],
-                                                            sys.argv[2],
-                                                            sys.argv[3]),
-                                                            pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    # Query all State objects and order by id
+    states = session.query(State).order_by(State.id).all()
+
+    # Print results as specified
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # Close the session
+    session.close()
