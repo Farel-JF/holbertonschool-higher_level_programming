7865532735d855175d08028f556444d9a9954e86
Farelito <bakoufarel@gmail.com>
2024-07-25 13:50:50 +0200

    4. Extending Dynamic Data Display to Include SQLite in Flask

--- python-server_side_rendering/task_04_db.py
+++ python-server_side_rendering/task_04_db.py
@@ -1,138 +1,71 @@
-#!/usr/bin/python3
+#!usr/bin/python3
+
 
 from flask import Flask, render_template, request
-from pathlib import Path
 import json
 import csv
 import sqlite3
 
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-@app.route('/items')
-def items():
-    items_list = []
+def read_json():
+    with open('products.json', 'r') as f:
+        return json.load(f)
 
-    with open("items.json", 'r') as f:
-        rows = json.load(f)
-    for key,value in rows.items():
-        items_list = value
 
-    return render_template('items.html', items=items_list)
+def read_csv():
+    products = []
+    with open('products.csv', 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            row['id'] = int(row['id'])
+            row['price'] = float(row['price'])
+            products.append(row)
+    return products
+
+
+def read_sql():
+    conn = sqlite3.connect('products.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT id, name, category, price FROM Products')
+    rows = cursor.fetchall()
+    conn.close()
+    products = []
+    for row in rows:
+        products.append({
+            'id': row[0],
+            'name': row[1],
+            'category': row[2],
+            'price': row[3]
+        })
+    return products
+
 
 @app.route('/products')
 def products():
     source = request.args.get('source')
-    id = request.args.get('id')
+    product_id = request.args.get('id', type=int)
 
-    data = []
-    if source == "json":
-        data = load_json_data("products.json", id)
-    elif source == "csv":
-        data = load_csv_data("products.csv", id)
-    elif source == "sql":
-        sql_filepath = "products.db"
-        # create sqlite file if it doesn't exist yet
-        if not Path(sql_filepath).is_file():
-            create_sql_data(sql_filepath)
+    if source == 'json':
+        products = read_json()
+    elif source == 'csv':
+        products = read_csv()
+    elif source == 'sql':
+        products = read_sql()
+    else:
+        return render_template('product_display.html', error="Wrong source")
 
-        data = load_sql_data(sql_filepath, id)
+    if product_id:
+        products = [
+            product for product in products if product['id'] == product_id]
+        if not products:
+            return render_template(
+                'product_display.html', error="Product not found")
 
-    return render_template('product_display.html', data=data, source=source, id=id)
-
-def load_json_data(filename, wanted_id = None):
-    """ Load JSON data from file and returns as dictionary """
-
-    data = []
-
-    if not Path(filename).is_file():
-        raise FileNotFoundError("Data file '{}' missing".format(filename))
-
-    try:
-        with open(filename, 'r') as f:
-            rows = json.load(f)
-        for row in rows:
-            key = row['id']
-
-            if (wanted_id is not None and key == wanted_id) or (wanted_id is None):
-                product = {}
-                for k,v in row.items():
-                    product[k] = v
-                data.append(product)
-
-    except ValueError as exc:
-        raise ValueError("Unable to load data from file '{}'".format(filename)) from exc
-
-    return data
-
-def load_csv_data(filename, wanted_id = None):
-    """ Load JSON data from file and returns as dictionary """
-
-    data = []
-
-    if not Path(filename).is_file():
-        raise FileNotFoundError("Data file '{}' missing".format(filename))
-
-    try:
-        with open(filename, 'r') as csvfile:
-            # using DictReader method to convert each row to a dictionary
-            for row in csv.DictReader(csvfile):
-                if (wanted_id is not None and row['id'] == wanted_id) or (wanted_id is None):
-                    data.append(row)
-    except ValueError as exc:
-        raise ValueError("Unable to load data from file '{}'".format(filename)) from exc
-
-    return data
-
-def load_sql_data(filename, wanted_id = None):
-    """ Load SQLite data and return as dictionary """
-
-    data = []
-    where_clause = ""
-    if wanted_id is not None:
-        where_clause = " WHERE id = " + wanted_id
-
-    con = sqlite3.connect(filename)
-    cur = con.cursor()
-    res = cur.execute("SELECT * FROM products " + where_clause)
-    rows = res.fetchall()
-
-    # longwinded way to get a list of the col names programatically
-    keys = []
-    colnames = cur.description
-    for desc in colnames:
-        keys.append(desc[0])
-
-    # keys = ["id", "name", "category", "price"]
-    for row_tuple in rows:
-        item = {}
-        i = 0
-        for v in row_tuple:
-            item[keys[i]] = v
-            i = i + 1
-        data.append(item)
-
-    # print(data)
-
-    return data
-
-def create_sql_data(filename):
-    """ Create SQLite data file if it doesn't already exist """
-
-    con = sqlite3.connect(filename)
-    cur = con.cursor()
-    cur.execute("CREATE TABLE products(id, name, category, price)")
-    cur.execute("""
-        INSERT INTO products VALUES
-            (1, "Laptop", "Electronics", 799.99),
-            (2, "Coffee Mug", "Home Goods", 15.99)
-    """)
-    con.commit()
+    return render_template('product_display.html', products=products)
 
 
-# Set debug=True for the server to auto-reload when there are changes
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(debug=True, port=5000)
